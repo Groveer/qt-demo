037149732134ba22a1dbb093a64e2469b74728fc
guoyao <guoyao@uniontech.com>
2022-01-07 20:32:10 +0800

    feat: add qml

--- temp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <deepin_pw_check.h>
-
-int main(int argc, char **argv)
-{
-    PW_ERROR_TYPE error = deepin_pw_check("root", "uos123!!", LEVEL_STRICT_CHECK, nullptr);
-    printf("error:%d", error);
-    return 0;
-}
