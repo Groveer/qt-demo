62fcec7b696223a197d39d40eb51fdce260be81b
groveer <groveer@qq.com>
2023-05-10 10:49:32 +0800

    chore: update memory-leak

--- memory-leak/main.cpp
+++ memory-leak/main.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+void manual_malloc()
 {
     char *p = nullptr;
     while (true) {
@@ -20,5 +21,29 @@ int main(int argc, char *argv[])
     }
     // 释放内存
     free(p);
+}
+
+void auto_malloc()
+{
+    char *p = nullptr;
+    unsigned long i = 20000;
+    i *= 1024 * 1024;
+    p = (char *)malloc(i * sizeof(char));
+    memset(p, 65, i);
+    while (true) {
+        unsigned long n = 100;
+        n *= 1024 * 1024;
+        // 动态申请内存
+        p = (char *)malloc(n * sizeof(char));
+        memset(p, 65, n);
+        sleep(3);
+    }
+    // 释放内存
+    free(p);
+}
+
+int main(int argc, char *argv[])
+{
+    auto_malloc();
     return 0;
 }
