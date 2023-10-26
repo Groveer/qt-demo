4940eef40ea02ecfcb3a137b92bad35c0427e701
groveer <groveer@qq.com>
2023-10-26 17:52:54 +0800

    feat: add networkmanager

--- network-check/main.cpp
+++ network-check/main.cpp
@@ -2,7 +2,11 @@
 #include <QDebug>
 #include <QLoggingCategory>
 
+#include <dactiveconnection.h>
+#include <dconnectionsetting.h>
+#include <dipv4setting.h>
 #include <dnetworkmanager.h>
+#include <dsettingsmanager.h>
 
 #include "qloggingcategory.h"
 
@@ -17,6 +21,36 @@ int main(int argc, char **argv) {
 
   QList<quint64> deviceList = networkManager.getDeviceIdList().value();
   qCDebug(app_main) << deviceList;
+  auto primaryConnection = networkManager.primaryConnection();
+  qCDebug(app_main) << primaryConnection;
+  auto activeConn = networkManager.getActiveConnectionObject(primaryConnection);
+  if (!activeConn) {
+    qCDebug(app_main) << activeConn.error();
+  }
+  const auto &conn = activeConn.value();
+  const auto &uuid = conn->UUID();
+
+  DSettingsManager settingsManager;
+  auto setting = settingsManager.getConnectionByUUID(uuid);
+
+  if (!setting) {
+    qCDebug(app_main) << setting.error();
+  }
+
+  DConnectionSetting connectionSetting(setting.value());
+
+  qDebug() << connectionSetting.settings().value();
+
+  // DIpv4Setting ipv4Setting;
+  // ipv4Setting.fromMap(connectionSetting.settings().value()["ipv4"]);
+  //
+  // qDebug() << (int)ipv4Setting.method();
+
+  // qCDebug(app_main) << active;
+  // qCDebug(app_main) << active;
+  // auto ipv4config_id = active->IP4Config();
+  // DIPv4Config ipv4config = DIPv4Config(ipv4config_id);
+  // qCDebug(app_main) << ipv4config.;
 
   app.exec();
 }
